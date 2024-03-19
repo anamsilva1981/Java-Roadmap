3c84f4bf104dda26e287189885b333a41bbf7c37
ana-msilva <ana.msilva@ibm.com>
2024-03-19 13:12:14 -0300

    feat: Poo Fundamentos - Tipos de Dados Primitivos

---
+++ src/poo-fundamentos/tiposDeDados/App.java
@@ -0,0 +1,23 @@
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("POO com Java");
+    
+        // TIPOS DE DADOS PRIMITIVOS (Sem a necessidade de importar nenhuma biblioteca)
+
+        byte tipoByte = 127; // Armazena de -127 até 127
+        Short tipoShort = 32737; // maior numero aceito
+        Integer tipoInteger = Integer.MAX_VALUE; // ate 2147483647
+        Long tipoLong = Long.MAX_VALUE; // TODO número LONG no java tem que ter a letra L no final
+        Float tipoFloat = Float.MAX_VALUE; // até 3.4028235E38 TODO número FLOAT no java tem que ter a letra "F" no final
+        Double tipoDouble = Double.MAX_VALUE; // armazena até 1.7976931348623157E308
+
+        String tipoString = "Armazena texto";
+        char tipoChat = 'S'; // Armazena apenas um caracter e tem que estar em aspas simples.
+        Boolean tipoBoolean = true; // verdeiro ou falso
+    }
+    
+    
+}
